a2dbef60e013901ae7e96f764dd198243293813a
Omendiff <kanhy-wp19@student.tarc.edu.my>
2021-02-21 22:41:31 +0800

    com

---
+++ chatbot.js
@@ -0,0 +1,7 @@
+function chatbot() {
+  document.getElementById("mainchatbox").style.display = "block";
+}
+
+function closebot() {
+  document.getElementById("mainchatbox").style.display = "none";
+}
---
+++ topbutton.js
@@ -0,0 +1,17 @@
+var topbutton = document.getElementById("topbtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
