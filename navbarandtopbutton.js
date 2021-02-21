3bc42cbf72ed3601728f54e04bbeef6588cf3d42
James-moneyMaker <longsa-wp19@student.tarc.edu.my>
2021-02-22 00:29:23 +0800

    hi

--- navbarandtopbutton.js
+++ navbarandtopbutton.js
@@ -1,6 +1,7 @@
+/*back to top button*/
 topbutton = document.getElementById("topbtn");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction(),myFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -14,4 +15,18 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+
+
+/*sticky navbar*/
+var navbar = document.getElementById("lower");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
